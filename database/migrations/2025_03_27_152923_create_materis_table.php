b1b1f3557b4b4b4f6a4a5626a8b14e718e04fcb3
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-27 15:33:07 +0000

    migrate materi tabel

--- database/migrations/2025_03_27_152923_create_materis_table.php
+++ database/migrations/2025_03_27_152923_create_materis_table.php
@@ -4,24 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('materis', function (Blueprint $table) {
+        Schema::create('materi', function (Blueprint $table) {
             $table->id();
+            $table->string('judul_materi');
+            $table->string('mata_pelajaran');
+            $table->text('deskripsi');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Menghubungkan ke tabel users
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('materis');
+        Schema::dropIfExists('materi');
     }
 };
