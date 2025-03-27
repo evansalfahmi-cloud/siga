21a40d8ddd9f92e5d90f2316de984e57b2610559
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-27 15:29:37 +0000

    menambahkan tabel materi

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Materi;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $materi = Materi::all(); // Ambil semua data
+        return view('dashboard', compact('materi'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'deskripsi' => 'required'
+        ]);
+
+        Materi::create([
+            'judul' => $request->judul,
+            'deskripsi' => $request->deskripsi
+        ]);
+
+        return redirect()->route('dashboard')->with('success', 'Materi berhasil ditambahkan.');
+    }
 }
