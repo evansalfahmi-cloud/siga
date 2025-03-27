bb72c17821790a65149540e97e99fd3530b65a23
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-27 16:01:07 +0000

    fix dashboard models, control and table

--- app/Models/Materi.php
+++ app/Models/Materi.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Materi extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'materi';
+
+    protected $fillable = [
+        'judul_materi',
+        'mata_pelajaran',
+        'deskripsi',
+        'user_id',
+    ];
+
+    // Relasi ke User (Guru)
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
