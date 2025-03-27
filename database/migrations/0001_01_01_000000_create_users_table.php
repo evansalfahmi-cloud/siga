1d37616a3e7dcf4daf0dc8e40db7892d3581fa86
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-27 14:55:52 +0000

    add boostrap, prepare database user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique(); // Menggunakan username, bukan email
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['tendik', 'siswa']); // Menentukan jenis pengguna
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('username')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
