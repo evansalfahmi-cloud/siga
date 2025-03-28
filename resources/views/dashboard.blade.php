5a4cce3bda447fe743331b92ff26a8b697a86c12
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-28 03:39:46 +0000

    tambah font baru untuk font default web

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     @vite(['resources/js/app.js'])
 </head>
 <body class="container mt-5">
